f70837ee5331012b4554c538947fcecdb48ea75a
Craig Loftus <craig@regulusweb.com>
2016-06-21 21:46:42 +0100

    Cache api response

--- oxfordpython/meetup/context_processors.py
+++ oxfordpython/meetup/context_processors.py
@@ -1,8 +1,15 @@
 from django.conf import settings
+from django.core.cache import cache
 
 import meetup.api
 
+MEETUP_CACHE_KEY = 'meetup_getgroup'
+
 
 def meetup_group(request):
-    mclient = meetup.api.Client(settings.MEETUP_API_KEY)
-    return {'meetup': mclient.GetGroup({'urlname': 'oxfordpython'})}
+    group = cache.get(MEETUP_CACHE_KEY)
+    if not group:
+        mclient = meetup.api.Client(settings.MEETUP_API_KEY)
+        group = mclient.GetGroup({'urlname': 'oxfordpython'})
+        cache.set(MEETUP_CACHE_KEY, group, 600) # Cache for 10mins, because caching
+    return {'meetup': group}
