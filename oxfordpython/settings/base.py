8a7b03e427cfaf33536626ba772c808dde3b2515
Piers Storey <piers.storey@gmail.com>
2016-06-25 22:24:14 +0100

    Single page template added to main index.html

--- oxfordpython/settings/base.py
+++ oxfordpython/settings/base.py
@@ -15,6 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '=+)7hj45f701_228*u+r%+%u6qe0ayrbh&uu7w$bxiphjab(6y'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
--- oxfordpython/urls.py
+++ oxfordpython/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from main import views as main_views
+from .main import views as main_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
